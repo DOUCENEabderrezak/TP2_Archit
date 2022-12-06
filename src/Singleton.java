5fb8b45a05ea438e2868859f23e36ecada30ffd2
DOUCENEabderrezak <119892321+DOUCENEabderrezak@users.noreply.github.com>
2022-12-06 13:46:21 +0100

    Update Singleton.java

--- src/Singleton.java
+++ src/Singleton.java
@@ -2,7 +2,7 @@ public  class Singleton {
 	private static Singleton instance; 
 	private Singleton() {}
 	public static Singleton getInstance() {
-		if (instance == nul) { 
+		if (instance == null) { 
 			instance = new Singleton();
 		}
 		return instance;
